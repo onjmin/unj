8cf425230289eedd78e4eef63ccb290c418a3236
onjmin <onjmin@example.com>
2025-02-12 23:55:27 +0900

    フロントで定数の参照ができるように

--- src/client/App.svelte
+++ src/client/App.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { UNJ_API_SECRET_PEPPER } from "./mylib/env";
+
   let count = 0;
 
   const increment = () => {
@@ -9,6 +11,12 @@
 <main>
   <h1>Counter App with Svelte + Express</h1>
   <p>Count: {count}</p>
+  <button on:click={increment}>Increment</button>
+</main>
+
+<main>
+  <h1>Counter App with Svelte + Express</h1>
+  <p>Count: {UNJ_API_SECRET_PEPPER}</p>
   <button on:click={increment}>Increment</button>
 </main>
 
--- src/client/mylib/env.ts
+++ src/client/mylib/env.ts
@@ -1,3 +1,5 @@
-export const DEV_MODE = process.env.DEV_MODE === "true";
-export const UNJ_API_URL = process.env.UNJ_API_URL ?? "http://localhost:3000";
-export const UNJ_API_SECRET_PEPPER = process.env.UNJ_API_SECRET_PEPPER ?? "";
+export const DEV_MODE = import.meta.env.VITE_DEV_MODE === "true";
+export const UNJ_API_URL =
+	import.meta.env.VITE_UNJ_API_URL ?? "http://localhost:3000";
+export const UNJ_API_SECRET_PEPPER =
+	import.meta.env.VITE_UNJ_API_SECRET_PEPPER ?? "";
