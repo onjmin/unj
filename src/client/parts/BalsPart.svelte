aa0e160c0f02164c385c57fa61ca42c5e9376e11
onjmin <onjmin@example.com>
2025-03-21 00:09:04 +0900

    バルス

--- src/client/parts/BalsPart.svelte
+++ src/client/parts/BalsPart.svelte
@@ -1,17 +1,42 @@
 <script lang="ts">
+  import { randArray } from "../../common/util.js";
+  import { visible } from "../mylib/dom.js";
+
   const s = document.createElement("script");
-  s.src = "https://code.jquery.com/jquery-3.7.1.slim.min.js";
-  s.integrity = "sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8=";
+  s.src = "https://code.jquery.com/jquery-1.12.4.min.js";
+  s.integrity = "sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=";
   s.crossOrigin = "anonymous";
-  s.onload = () => {
-    const s = document.createElement("script");
-    s.src = "https://furage.github.io/valus/main.js";
-    document.body.appendChild(s);
-  };
   document.body.appendChild(s);
 </script>
 
-<div class="valus_res">
+<div
+  class="valus_res"
+  use:visible={(visible) => {
+    if (visible) {
+      const $ = (window as any).$;
+      $(".valus_res").addClass(
+        randArray([
+          "shake-chunk",
+          "shake-opacity",
+          "shake-crazy",
+          "shake-hard",
+        ]),
+      );
+      setTimeout(function () {
+        $("html").addClass(
+          randArray(["shake", "shake-slow", "shake-little", "shake-vertical"]),
+        );
+        $(".main-container")
+          .add($(".article-body-outer"))
+          .css("background", "#FFCCCC");
+        const s = document.createElement("script");
+        s.src = "https://furage.github.io/valus/bomb.v3.js";
+        s.crossOrigin = "anonymous";
+        document.body.appendChild(s);
+      }, 2500);
+    }
+  }}
+>
   <img
     src="https://livedoor.blogimg.jp/furage/imgs/8/4/84c5ac1b-s.png"
     alt="test"
