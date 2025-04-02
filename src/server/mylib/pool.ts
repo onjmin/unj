3cea52d76e09c9d1b4464e5dc0e0b973245ebdc3
onjmin <onjmin@example.com>
2025-04-02 09:44:18 +0900

    サーバーレスからpgへ

--- src/server/mylib/pool.ts
+++ src/server/mylib/pool.ts
@@ -1,10 +1,8 @@
-import { Pool, neonConfig } from "@neondatabase/serverless";
-import ws from "ws";
+import pg from "pg";
 import { logger } from "../mylib/log.js";
 
-neonConfig.webSocketConstructor = ws;
 export const NEON_DATABASE_URL = String(process.env.NEON_DATABASE_URL);
-export const pool = new Pool({ connectionString: NEON_DATABASE_URL });
+export const pool = new pg.Pool({ connectionString: NEON_DATABASE_URL });
 
 pool.on("error", (error) => {
 	logger.error(error);
