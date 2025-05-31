a97ecb1788fd1058fda46966f0740bc411780b75
onjmin <onjmin@example.com>
2025-05-31 18:32:01 +0900

    fix

--- workflow/finish.ts
+++ workflow/finish.ts
@@ -5,21 +5,21 @@ import { fileURLToPath } from "node:url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const dir = path.resolve(__dirname, "..");
-const sourcePath1 = path.join(dir, "static");
-const sourcePath2 = path.join(dir, "src", "client", "_redirects");
+const sourceDir1 = path.join(dir, "static");
+const sourceDir2 = path.join(dir, "src", "client");
 const destPath = path.join(dir, "dist", "client");
 
 try {
 	console.log("🤖", "dist/clientにstaticと_redirectsを転送");
 
-	await cp(sourcePath1, path.join(destPath, "static"), {
+	await cp(sourceDir1, path.join(destPath, "static"), {
 		recursive: true,
 	});
 	await cp(
-		path.join(sourcePath1, "sitemap.xml"),
-		path.join(destPath, "sitemap.xml"),
+		path.join(sourceDir2, "_redirects"),
+		path.join(destPath, "_redirects"),
 	);
-	await cp(sourcePath2, path.join(destPath, "_redirects"));
+	await cp(path.join(sourceDir2, "_headers"), path.join(destPath, "_headers"));
 	console.log("✅", "Folder copied successfully!");
 } catch (error) {
 	console.error("❌", "Failed to copy folder:", error);
