b5c81ab67d038f0cc1c59e39bf59caa1c6b8a7df
おんJ民 <J onjmin@users.noreply.github.com>
2025-09-01 23:06:46 +0900

    fix

--- src/client/pages/MisskeyPage.svelte
+++ src/client/pages/MisskeyPage.svelte
@@ -5,6 +5,9 @@
     import MainPart from "../parts/MainPart.svelte";
     ///////////////
 
+    import { CircleArrowLeftIcon } from "@lucide/svelte";
+    import { navigate } from "svelte-routing";
+    import { makePathname } from "../mylib/env.js";
     import {
         type Misskey,
         type Timeline,
@@ -162,6 +165,26 @@
             </button>
         </div>
     {/if}
+
+    <div
+        class="flex flex-col space-y-2 p-4 bg-gray-800 text-gray-200 rounded-lg"
+    >
+        <button
+            class="flex items-center space-x-2 px-4 py-2 rounded-lg bg-gray-600 hover:bg-gray-700 transition-colors duration-200"
+            onclick={() => navigate(makePathname("/headline"))}
+        >
+            <CircleArrowLeftIcon size={16} />
+            <span class="text-sm font-medium">板トップに戻る</span>
+        </button>
+
+        <button
+            class="flex items-center space-x-2 px-4 py-2 rounded-lg bg-gray-600 hover:bg-gray-700 transition-colors duration-200"
+            onclick={() => navigate(makePathname("/history"))}
+        >
+            <CircleArrowLeftIcon size={16} />
+            <span class="text-sm font-medium">履歴に戻る</span>
+        </button>
+    </div>
 </MainPart>
 
 <FooterPart />
