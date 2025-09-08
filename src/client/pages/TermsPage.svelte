967e71108e32c48eb3b3ec7e7e80f0c1295892a6
おんJ民 <J onjmin@users.noreply.github.com>
2025-09-08 22:50:30 +0900

    fix

--- src/client/pages/TermsPage.svelte
+++ src/client/pages/TermsPage.svelte
@@ -5,35 +5,54 @@
     import MainPart from "../parts/MainPart.svelte";
     ///////////////
 
-    import Accordion, { Panel, Header, Content } from "@smui-extra/accordion";
+    import { ChevronDownIcon, ChevronRightIcon } from "@lucide/svelte";
     import { faq } from "../mylib/faq.js";
     import TermsPart from "../parts/TermsPart.svelte";
+
+    // 開いている質問のインデックスを管理する状態
+    let openQuestion: number | null = $state(null);
+
+    // 質問の開閉を切り替える関数
+    const toggleQuestion = (index: number) => {
+        openQuestion = openQuestion === index ? null : index;
+    };
 </script>
 
 <HeaderPart title="うんｊ利用規約">
     <p>よくある質問</p>
-    <div class="accordion-container">
-        <Accordion>
-            {#each faq as [q, a]}
-                <Panel>
-                    <Header>{q}</Header>
-                    <Content>{a}</Content>
-                </Panel>
-            {/each}
-        </Accordion>
-    </div></HeaderPart
->
+    <div class="space-y-4">
+        {#each faq as [q, a], i}
+            <div class="bg-white dark:bg-gray-800 rounded-lg shadow">
+                <div
+                    tabindex="0"
+                    role="button"
+                    onkeydown={() => {}}
+                    class="flex justify-between items-center p-4 cursor-pointer"
+                    onclick={() => toggleQuestion(i)}
+                >
+                    <h3 class="font-bold text-lg">{q}</h3>
+                    {#if openQuestion === i}
+                        <ChevronDownIcon class="h-6 w-6" />
+                    {:else}
+                        <ChevronRightIcon class="h-6 w-6" />
+                    {/if}
+                </div>
+                {#if openQuestion === i}
+                    <div
+                        class="p-4 border-t border-gray-200 dark:border-gray-700"
+                    >
+                        {a}
+                    </div>
+                {/if}
+            </div>
+        {/each}
+    </div>
+</HeaderPart>
 
 <MainPart>
-    <div class="unj-terms">
+    <div class="unj-terms text-left">
         <TermsPart />
     </div>
 </MainPart>
 
 <FooterPart />
-
-<style>
-    :global(.unj-terms pre) {
-        text-align: left;
-    }
-</style>
