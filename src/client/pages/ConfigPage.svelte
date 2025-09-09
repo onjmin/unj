084b5882da3a29b62a36c7aecebcd5985f2cb9df
おんJ民 <J onjmin@users.noreply.github.com>
2025-09-10 01:31:38 +0900

    fix

--- src/client/pages/ConfigPage.svelte
+++ src/client/pages/ConfigPage.svelte
@@ -336,6 +336,45 @@
                             <div>お絵描きをうｐしてから出直してね。</div>
                         </div>
                     {:else}
+                        {#snippet paginationControls()}
+                            {#if totalPages > 1}
+                                <div
+                                    class="flex justify-center items-center mt-4 space-x-2"
+                                >
+                                    <button
+                                        onclick={() => (currentPage = 1)}
+                                        disabled={currentPage === 1}
+                                        class="px-3 py-1 rounded-md bg-gray-200 hover:bg-gray-300 disabled:opacity-50"
+                                    >
+                                        最初へ
+                                    </button>
+                                    <button
+                                        onclick={() => currentPage--}
+                                        disabled={currentPage === 1}
+                                        class="px-3 py-1 rounded-md bg-gray-200 hover:bg-gray-300 disabled:opacity-50"
+                                    >
+                                        前へ
+                                    </button>
+                                    <span>{currentPage} / {totalPages}</span>
+                                    <button
+                                        onclick={() => currentPage++}
+                                        disabled={currentPage === totalPages}
+                                        class="px-3 py-1 rounded-md bg-gray-200 hover:bg-gray-300 disabled:opacity-50"
+                                    >
+                                        次へ
+                                    </button>
+                                    <button
+                                        onclick={() =>
+                                            (currentPage = totalPages)}
+                                        disabled={currentPage === totalPages}
+                                        class="px-3 py-1 rounded-md bg-gray-200 hover:bg-gray-300 disabled:opacity-50"
+                                    >
+                                        最後へ
+                                    </button>
+                                </div>
+                            {/if}
+                        {/snippet}
+                        {@render paginationControls()}
                         <div class="text-left space-y-4">
                             {#each paginatedImgurList as imgurResponse}
                                 <div
@@ -411,41 +450,7 @@
                                 </div>
                             {/each}
                         </div>
-                        {#if totalPages > 1}
-                            <div
-                                class="flex justify-center items-center mt-4 space-x-2"
-                            >
-                                <button
-                                    onclick={() => (currentPage = 1)}
-                                    disabled={currentPage === 1}
-                                    class="px-3 py-1 rounded-md bg-gray-200 hover:bg-gray-300 disabled:opacity-50"
-                                >
-                                    最初へ
-                                </button>
-                                <button
-                                    onclick={() => currentPage--}
-                                    disabled={currentPage === 1}
-                                    class="px-3 py-1 rounded-md bg-gray-200 hover:bg-gray-300 disabled:opacity-50"
-                                >
-                                    前へ
-                                </button>
-                                <span>{currentPage} / {totalPages}</span>
-                                <button
-                                    onclick={() => currentPage++}
-                                    disabled={currentPage === totalPages}
-                                    class="px-3 py-1 rounded-md bg-gray-200 hover:bg-gray-300 disabled:opacity-50"
-                                >
-                                    次へ
-                                </button>
-                                <button
-                                    onclick={() => (currentPage = totalPages)}
-                                    disabled={currentPage === totalPages}
-                                    class="px-3 py-1 rounded-md bg-gray-200 hover:bg-gray-300 disabled:opacity-50"
-                                >
-                                    最後へ
-                                </button>
-                            </div>
-                        {/if}
+                        {@render paginationControls()}
                     {/if}
                 </div>
             {/if}
