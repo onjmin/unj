fa7026e0f40d86a1a7278b945cd5006354a85c7d
おんJ民 <J onjmin@users.noreply.github.com>
2025-09-16 05:08:30 +0900

    fix

--- src/client/parts/FooterPart.svelte
+++ src/client/parts/FooterPart.svelte
@@ -7,7 +7,7 @@
     <div class="unj-director">
       <hr />
       <a
-        href="https://ja.wikipedia.org/wiki/%E3%82%B3%E3%83%94%E3%83%BC%E3%83%AC%E3%83%95%E3%83%88"
+        href="https://ja.wikipedia.org/wiki/%E8%87%AA%E7%94%B1%E8%8A%B8%E8%A1%93%E3%83%A9%E3%82%A4%E3%82%BB%E3%83%B3%E3%82%B9"
         target="_blank"
         rel="noopener noreferrer">著作権コピーレフト</a
       >：うんｊはまとめ自由
