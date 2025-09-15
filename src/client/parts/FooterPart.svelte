42bb07df67870c79a42b3bed195626739c974ada
おんJ民 <J onjmin@users.noreply.github.com>
2025-09-16 05:11:46 +0900

    fix

--- src/client/parts/FooterPart.svelte
+++ src/client/parts/FooterPart.svelte
@@ -4,8 +4,8 @@
 
 <footer class="unj-footer-part">
   {#if !menu}
-    <div class="unj-director">
-      <hr />
+    <div class="text-center opacity-40">
+      <hr class="opacity-20" />
       <a
         href="https://ja.wikipedia.org/wiki/%E8%87%AA%E7%94%B1%E8%8A%B8%E8%A1%93%E3%83%A9%E3%82%A4%E3%82%BB%E3%83%B3%E3%82%B9"
         target="_blank"
@@ -14,13 +14,3 @@
     </div>
   {/if}
 </footer>
-
-<style>
-  .unj-director {
-    text-align: center;
-    opacity: 0.4;
-  }
-  .unj-director hr {
-    opacity: 0.2;
-  }
-</style>
