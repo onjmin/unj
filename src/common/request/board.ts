9289fe8f999385eb357563c19c9664a8d71aa82d
おんJ民 <J onjmin@users.noreply.github.com>
2025-12-30 07:10:01 +0900

    fix

--- src/common/request/board.ts
+++ src/common/request/board.ts
@@ -60,7 +60,7 @@ export const unjBoard = new Board({
 	description: "運営と運命を共にする、うんち実況（セーラージュピター）",
 	// banner: "https://i.imgur.com/O1A36k3.png",
 	// banner: "https://pub-d049c945dab44db6b75372fdf9cb8401.r2.dev/69c4be44.png",
-	banner: "https://i.imgur.com/6x0RndJ.png",
+	banner: "https://pub-d049c945dab44db6b75372fdf9cb8401.r2.dev/bc307e45.png",
 });
 for (const v of unjAvatars) unjBoard.avatarMap.set(v.id, v);
 export const noharaBoard = new Board({
